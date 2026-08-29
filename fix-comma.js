51603f7f1eaabd286a1d0f5d581a0a3047187758
abdelrahmanyasser0 <abdelrahmanbody775@gmail.com>
2026-08-29 19:46:18 +0300

    chore: remove helper scripts

--- fix-comma.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('D:\\\\android tog\\\\codely\\\\lib\\\\data\\\\initialData.ts', 'utf-8');
-c = c.replace(/\],\s*\},\s*,\s*\{\s*slug: "edubridge"/g, '],\n  },\n  {\n    slug: "edubridge"');
-fs.writeFileSync('D:\\\\android tog\\\\codely\\\\lib\\\\data\\\\initialData.ts', c);
-console.log('done');
