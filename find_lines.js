432511d7a36b1cfb97607a4c44c645a26f955d51
abdelrahmanyasser0 <abdelrahmanbody775@gmail.com>
2026-08-30 03:11:44 +0300

    chore: remove scratch scripts

--- find_lines.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('lib/data/initialData.ts', 'utf-8').split('\n');
-lines.forEach((l, i) => { 
-  if (l.includes('slug: "edubridge"')) console.log(i + 1, l); 
-  if (l.includes('title_en: "Deliver It"')) console.log(i + 1, l); 
-  if (l.includes('coverImage') && (l.includes('deliver') || l.includes('edubridge'))) console.log(i + 1, l);
-});
--- get_slugs.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('lib/data/initialData.ts', 'utf-8');
-const slugs = content.match(/slug:\s*['"](.*?)['"]/g);
-console.log(slugs);
